adb33a959d3da0871fbbb59a63a6ed87196421ea
orlandojtx <46915982+orlandojtx@users.noreply.github.com>
2021-10-28 13:00:54 -0500

    update frontend template

---
+++ app.js
@@ -0,0 +1,16 @@
+const routes=[
+    {path:'/pais',component:pais},
+    {path:'/vacuna',component:vacuna},
+    {path:'/tipodoc',component:tipodoc},
+    {path:'/persona',component:persona},
+    {path:'/solicitud',component:solicitud},
+    {path:'/vacunaacep',component:vacunaacep},
+]
+
+const router=new VueRouter({
+    routes
+})
+
+const app = new Vue({
+    router
+}).$mount('#app')
